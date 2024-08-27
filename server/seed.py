e2d4418a34417e09f8835cf925d0bdf27da3855d
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-08-28 01:55:15 +0300

    done

--- server/seed.py
+++ server/seed.py
@@ -6,3 +6,26 @@ from faker import Faker
 
 from app import app
 from models import db, Owner, Pet
+
+db.init_app(app)
+
+fake = Faker()
+
+with app.app_context():
+    Pet.query.delete()
+    Owneer.query.delete()
+
+    owners = []
+    for n in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
+
+    db.session.add_all(owners)
+
+    pets = []
+    for n in range(50):
+        pet = Pet(name=fake.name())
+        pets.append(pet)
+
+    db.session.add_all(pets)
+    db.session.commit()
